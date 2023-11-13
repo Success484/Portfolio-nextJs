5311d282a10eb3aca190fb9f5b3c4d451abe0617
Success484 <successsimeon484@gmail.com>
2023-11-13 18:45:28 +0100

    feature: portfolio

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -10,11 +10,11 @@ export default function ProjectCard({imgUrl, title, description, gitUrl, preview
         style={{background: `url(${imgUrl})`,
         backgroundSize: "cover"}}>
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
-            <Link href={gitUrl} className='h-14 w-14 mr-2 relative rounded-full border-2 border-[#adb7be] hover:border-white flex items-center justify-center group/link'>
+            <Link href={gitUrl} target='_blank' className='h-14 w-14 mr-2 relative rounded-full border-2 border-[#adb7be] hover:border-white flex items-center justify-center group/link'>
                 <CodeBracketIcon className='h-10 w-10 text-[#adb7be] cursor-pointer group-hover/link:text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' />
             </Link>
 
-            <Link href={previewUrl} className='h-14 w-14 relative rounded-full border-2 border-[#adb7be] hover:border-white flex items-center justify-center group/link'>
+            <Link href={previewUrl} target='_blank' className='h-14 w-14 relative rounded-full border-2 border-[#adb7be] hover:border-white flex items-center justify-center group/link'>
                 <EyeIcon className='h-10 w-10 text-[#adb7be] cursor-pointer group-hover/link:text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' />
             </Link>
         </div>
